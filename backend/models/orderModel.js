2c0eb062d0029f2af8208646cc08978917527528
Olesya <oleoleksjuk@gmail.com>
2021-12-25 23:57:25 +0300

    create models

---
+++ backend/models/orderModel.js
@@ -0,0 +1,81 @@
+import mongoose from 'mongoose';
+
+const orderSchema = mongoose.Schema({
+  // adds a relationship between the order and the user (who made it)
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  // an array of ordered items
+  orderItems: [
+    {
+      name: { type : String, required: true },
+      quantity: { type : Number, required: true },
+      image: { type : String, required: true },
+      price: { type : Number, required: true },
+      // adds a relationship between the order and the products
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Product',
+      }
+    }
+  ],
+  // field as an embedded object
+  shippingAddress: {
+    address: { type: String, required : true, },
+    city: { type: String, required: true, },
+    postalCode: { type: String, required: true, },
+    country: { type: String, required: true, },
+  },
+  // PayPal gateway (scalable)
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  // data come from PayPal
+  paymentResult: {
+    id: { type: String },
+    status: { type: String },
+    update_time: { type: String },
+    email_address: { type: String }
+  },
+  taxPrice: {
+    type: Number,
+    required: true,
+    default: 0.0
+  },
+  shippingPrice: {
+  type: Number,
+  required: true,
+  default: 0.0
+},
+  totalPrice: {
+  type: Number,
+  required: true,
+  default: 0.0
+},
+  isPaid: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  paidAt: {
+    type: Date,
+  },
+  isDelivered: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  deliveredAt: {
+    type: Date,
+  },
+}, {
+  timestamps: true
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
---
+++ backend/models/productModel.js
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose';
+
+// individual review rating
+const reviewSchema = mongoose.Schema({
+  name: { type : String, required: true },
+  rating: { type : Number, required: true },
+  comment: { type : String, required: true },
+},{
+  timestamps: true,
+})
+
+const productSchema = mongoose.Schema({
+  // adds a relationship between the product and the user (admin who added it)
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    // reference a specific model for this ObjectId
+    ref: 'User',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  reviews: [reviewSchema],
+  // average numeric rating of all the review ratings
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  // number of reviews
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+}, {
+  timestamps: true
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
