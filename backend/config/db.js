4944502bbd74c74f168a6e2e3f43fe3e984ed949
Olesya <oleoleksjuk@gmail.com>
2022-03-04 00:22:05 +0300

    database connection file

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const connectDB = async (uri, callback) => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      autoIndex: true
+    });
+
+    console.log(`MongoDB Connected: ${mongoose.connection.host}`.cyan.underline.bold);
+
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
