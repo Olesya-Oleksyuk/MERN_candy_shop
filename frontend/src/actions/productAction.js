ec0497f5489af3f65568d978d5edbb2b3d2c2c2b
Olesya <oleoleksjuk@gmail.com>
2022-03-19 15:51:03 +0300

    product details reducer

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from '../constants/productConstants';
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+} from '../constants/productConstants';
 
 // action creators
 export const listProducts = () => async (dispatch) => {
@@ -14,7 +21,30 @@ export const listProducts = () => async (dispatch) => {
     console.log(e.response.data.message);
     dispatch({
       type: PRODUCT_LIST_FAIL,
-      payload: e.response && e.response.data.message ? e.response.data.message : e.message,
+      payload:
+        e.response && e.response.data.message
+          ? e.response.data.message
+          : e.message,
+    });
+  }
+};
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+    const { data } = await axios.get(`/api/products/${id}`);
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (e) {
+    console.log(e.response.data.message);
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        e.response && e.response.data.message
+          ? e.response.data.message
+          : e.message,
     });
   }
 };
--- frontend/src/pages/Product/index.jsx
+++ frontend/src/pages/Product/index.jsx
@@ -1,95 +1,112 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import {
   Row, Col, Image, ListGroup, Card, Button, ListGroupItem,
 } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
-import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Rating from '../../components/Rating';
+import Loader from '../../components/Loader';
+import Message from '../../components/Message';
 import { toCurrency } from '../../helpers/data';
 
+import { listProductDetails } from '../../actions/productAction';
+import './style.scss';
+
 const Product = () => {
-  const [product, setProduct] = useState({});
+  const dispatch = useDispatch();
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+  const currentProductId = productDetails.product._id;
+
   const { id: productId } = useParams();
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get(`/api/products/${productId}`);
-      setProduct(data);
-    };
-
-    fetchProducts();
+    dispatch(listProductDetails(productId));
   }, []);
 
-  // const product = products.find((i) => i._id === productId);
   const price = toCurrency(product.price, 'USD', 'en-US');
+
+  const getContent = () => {
+    if (loading || (currentProductId !== productId)) {
+      return <Loader />;
+    }
+    if (error) {
+      return (<Message variant="danger">{error}</Message>);
+    }
+    if (product._id === productId) {
+      return (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating reviewsNumber={product.numReviews} ratingValue={product.rating} />
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <div>
+                  Цена:
+                  {' '}
+                  {price}
+                </div>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <div>
+                  Описание:
+                  {' '}
+                  {product.description}
+                </div>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroupItem>
+                  <Row>
+                    <Col>
+                      Цена:
+                    </Col>
+                    <Col>
+                      <strong>
+                        {price}
+                      </strong>
+                    </Col>
+                  </Row>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <Row>
+                    <Col>
+                      Статус:
+                    </Col>
+                    <Col>
+                      {product.countInStock > 0 ? 'В наличии' : 'Нет в наличии'}
+                    </Col>
+                  </Row>
+                </ListGroupItem>
+                <ListGroupItem className="d-grid" disabled={!product.countInStock}>
+                  <Button type="button">
+                    Добавить в корзину
+                  </Button>
+                </ListGroupItem>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      );
+    }
+    return <></>;
+  };
 
   return (
     <>
-      <Link className="btn btn-light my-3" to="/">Вернуться</Link>
-      <Row>
-        <Col md={6}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating reviewsNumber={product.numReviews} ratingValue={product.rating} />
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <div>
-                Цена:
-                {' '}
-                {price}
-              </div>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <div>
-                Описание:
-                {' '}
-                {product.description}
-              </div>
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
-            <ListGroup variant="flush">
-              <ListGroupItem>
-                <Row>
-                  <Col>
-                    Цена:
-                  </Col>
-                  <Col>
-                    <strong>
-                      {price}
-                    </strong>
-                  </Col>
-                </Row>
-              </ListGroupItem>
-              <ListGroupItem>
-                <Row>
-                  <Col>
-                    Статус:
-                  </Col>
-                  <Col>
-                    {product.countInStock > 0 ? 'В наличии' : 'Нет в наличии'}
-                  </Col>
-                </Row>
-              </ListGroupItem>
-              <ListGroupItem className="d-grid" disabled={!product.countInStock}>
-                <Button type="button">
-                  Добавить в корзину
-                </Button>
-              </ListGroupItem>
-            </ListGroup>
-          </Card>
-        </Col>
-
-      </Row>
+      <Link className="btn btn-light my-3 return" to="/">Вернуться</Link>
+      {getContent()}
     </>
   );
 };
