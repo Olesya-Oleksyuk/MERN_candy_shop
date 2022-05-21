1239368c6b0a760bf60c522e451910c1063c09b4
Olesya <oleoleksjuk@gmail.com>
2022-05-21 20:36:00 +0300

    User Registration & Password Encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,12 +2,11 @@ import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 import generateToken from '../utils/generateToken.js';
 
-// @desc Аутентификация пользователя и токен
+// @desc Аутентификация пользователя и генерация токена
 // @route POST /api/users/login
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-  // res.send({ email, password, })
 
   // Аутентификация email & password
   try {
@@ -29,6 +28,40 @@ const authUser = asyncHandler(async (req, res) => {
   } else {
     res.status(401);
     throw new Error('Invalid email or password')
+  }
+
+});
+
+// @desc Регистрация пользователя
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExist = await User.findOne({email});
+
+  if (userExist) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (newUser) {
+    res.status(201).json({
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: generateToken(newUser._id),
+    })
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
   }
 
 });
@@ -56,4 +89,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,7 +31,18 @@ const userSchema = mongoose.Schema(
 // сопоставляем введеный пользователем пароль с зашифрованным паролем из БД
 userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
-}
+};
+
+// pre-save метод для хэширования строкового пароля при добавлении новой записи
+// модели в БД (при регистрации и при смене пароля)
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 // create model from the schema
 const User = mongoose.model('User', userSchema);
