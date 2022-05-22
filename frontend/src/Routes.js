32f0f350a060c3fda6624964936048c366b2bd77
Olesya <oleoleksjuk@gmail.com>
2022-05-22 17:24:51 +0300

    Add User Login Screen

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,17 +4,21 @@ import { Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Product from './pages/Product';
 import Cart from './pages/Cart';
+import LoginScreen from './pages/LoginScreen';
 
 const Routes = () => (
   <>
+    <Route path="/login" exact>
+      <LoginScreen />
+    </Route>
     <Route path="/product/:id" exact>
       <Product />
     </Route>
-    <Route path="/home" exact>
-      <Home />
-    </Route>
     <Route path="/cart/:id?">
       <Cart />
+    </Route>
+    <Route path="/home" exact>
+      <Home />
     </Route>
     <Route path="/" exact>
       <Home />
---
+++ frontend/src/pages/LoginScreen/index.jsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import {
+  Form, Button, Row, Col, FormGroup, FormLabel, FormControl,
+} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Message from '../../components/Message';
+import Loader from '../../components/Loader';
+import { login } from '../../actions/userActions';
+import FormContainer from '../../components/FormContainer';
+
+const LoginScreen = () => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+
+  const redirect = location.search ? location.search.split('=')[1] : '/';
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [dispatch, history, userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
+  const loginFormContent = useMemo(() => {
+    if (loading) {
+      return (
+        <>
+          <h1>Авторизация</h1>
+          <Loader />
+        </>
+      );
+    }
+
+    if (!userInfo) {
+      return (
+        <>
+          { error && <Message variant="danger">{error}</Message>}
+          <Form onSubmit={submitHandler}>
+            <FormGroup controlId="email" className="my-3">
+              <FormLabel>Электронная почта</FormLabel>
+              <FormControl type="email" placeholder="Введите email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </FormGroup>
+            <FormGroup controlId="password" className="my-3">
+              <FormLabel>Пароль</FormLabel>
+              <FormControl type="password" placeholder="Введите пароль" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </FormGroup>
+            <Button type="submit" variant="primary" className="mt-3">
+              Вход
+            </Button>
+          </Form>
+
+          <Row className="py-3">
+            <Col>
+              Впервые у нас?
+              <Link
+                to={redirect ? `/register?redirect=${redirect}` : '/register'}
+                style={{ marginLeft: '10px' }}
+                className="regular-link"
+              >
+                Зарегистрироваться
+              </Link>
+            </Col>
+          </Row>
+        </>
+      );
+    }
+
+    return <></>;
+  }, [userInfo]);
+
+  return (
+    <FormContainer>
+      {loginFormContent}
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
