77890a031f2d5062bf666196ad871d24fcd062ed
Olesya <oleoleksjuk@gmail.com>
2022-05-23 14:30:25 +0300

    small fix

--- frontend/src/components/CheckoutSteps/index.jsx
+++ frontend/src/components/CheckoutSteps/index.jsx
@@ -7,7 +7,7 @@ import './style.scss';
 const CheckoutSteps = ({
   step1, step2, step3, step4,
 }) => (
-  <Nav className="justify-content-center mb-4">
+  <Nav className="justify-content-evenly mb-4">
     <NavItem>
       {step1 ? (
         <LinkContainer to="/login">
