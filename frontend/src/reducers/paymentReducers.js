70cf9965e3820a2ac4c88195d962dcb0b3581667
Olesya <oleoleksjuk@gmail.com>
2022-06-28 12:54:49 +0300

    small fix

--- frontend/src/reducers/paymentReducers.js
+++ frontend/src/reducers/paymentReducers.js
@@ -3,7 +3,6 @@ import { SAVE_PAYMENT_METHOD } from '../constants/paymentConstants';
 export const paymentReducer = (state = { paymentMethod: {} }, action) => {
   switch (action.type) {
     case SAVE_PAYMENT_METHOD: {
-      console.log(action.payload);
       return {
         ...state,
         paymentMethod: action.payload,
