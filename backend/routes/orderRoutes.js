e070236f133742b6bbae82a89c10776a8ccb03d9
Olesya-Oleksyuk <olesyaoleksyuk@mail.ru>
2022-11-13 19:41:56 +0300

    add endpoint to mark an order as delivered  (back)

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import {
   addOrderItems,
-  getOrderById,
-  updateOrderToPaid,
   getMyOrders,
+  getOrderById,
   getOrders,
+  updateOrderToDelivered,
+  updateOrderToPaid
 } from '../controllers/orderController.js';
 import { isAdmin, protect } from '../middleware/authMiddleware.js';
 
@@ -14,5 +15,6 @@ router.route('/').post(protect, addOrderItems).get(protect, isAdmin, getOrders);
 router.route('/myorders').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/:id/deliver').put(protect, isAdmin, updateOrderToDelivered);
 
 export default router;
